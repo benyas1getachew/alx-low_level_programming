cc91babc005ad89efae507d53473bffc673c9b85
Benyas <benyas1getachew@gmail.com>
2022-03-10 14:51:36 -0800

    love

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints text using puts()
+ * main -prints text using puts()
  *
  * Return: Always 0 (Success)
  */
-#define RED "\e[0m"
 int main(void)
 {
-	puts("\"Programming is like buliding a multilingual puzzle");
-	if (0 > 1)
-	{
-		fprintf(stderr, "Anything");
-	}
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
