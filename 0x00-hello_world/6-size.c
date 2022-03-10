7006df575ce88b5ee83a282fe6ac1a15019988cb
Benyas <benyas1getachew@gmail.com>
2022-03-10 11:41:53 -0800

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * *main -main block
+ * Return:0
+ */
 int main(void)
 {
 int x;
@@ -8,7 +12,7 @@ char z;
 float w;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(z));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(w));
 return (0);
