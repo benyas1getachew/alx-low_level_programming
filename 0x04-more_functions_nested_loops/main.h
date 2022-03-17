1d4700cef96d327e076e72bc0e0cc7acc1171a4c
Benyas <benyas1getachew@gmail.com>
2022-03-17 21:18:02 +0530

    My commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 
 /*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x03-more_functions_nested_loops directory.
+ * File: main,h
  */
 
 int _putchar(char c);
